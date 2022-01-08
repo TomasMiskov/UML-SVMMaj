2ab8917a1fe52bea6325ebf5c41c2e9778d45312
Tomas Miskov <89302240+TomasMiskov@users.noreply.github.com>
2022-01-08 20:28:24 +0100

    Final update
    Thanks to Loek!
    Added:
    - quadratic hinge
    - treatment of the edge cases of the value of 'a' in the majorizing function (ax^2 + bx + c) with absolute hinge

--- SVMMaj_lib.R
+++ SVMMaj_lib.R
@@ -1,38 +1,37 @@
 # Name: SVMMaj_lib
-# Author: Tomas Miskov
-# Date: 04/01/22
-# Version: 1.0
+# Author: Tomas Miskov & Loek van Montfort
+# Date: 08/01/22
+# Version: 3.0
 # Purpose: Functions librarY for minimizing SVM loss using majorization
 #----------------------------------------------------------------------
 
-#' Absolute SVM Loss Function
+#' SVM Loss Function
 #' 
-#' Compute the absolute SVM loss function
+#' Compute the SVM loss function
 #' 
 #' @param mX Matrix of regressors including a column of 1s
 #' @param vY Vector of outcome values (2 categories, -1 & 1)
 #' @param vV Vector of weights (+ a constant)
 #' @param dLambda Ridge penalization parameter lambda
-AbsLossSVM <- function(mX, vY, vV, dLambda){
+LossSVM <- function(mX, vY, vV, dLambda){
   vQ <- mX %*% vV                                                         
   dLoss <- sum(pmax(0, 1 - vY * vQ)) + dLambda * (t(vV[-1]) %*% vV[-1])
   return(dLoss)
 }
 
-#' Quadratic SVM Loss Function
+#' SVM Quadratic Loss Function
 #' 
-#' Compute the quadratic SVM loss function
+#' Compute the SVM loss function for quadratic hinge
 #' 
 #' @param mX Matrix of regressors including a column of 1s
 #' @param vY Vector of outcome values (2 categories, -1 & 1)
 #' @param vV Vector of weights (+ a constant)
 #' @param dLambda Ridge penalization parameter lambda
-QuadLossSVM <- function(mX, vY, vV, dLambda){
+LossSVMQuadr <- function(mX, vY, vV, dLambda){
   vQ <- mX %*% vV                                                         
-  dLoss <- sum(pmax(0, 1 - vY * vQ)^2) + dLambda * (t(vV[-1]) %*% vV[-1])
+  dLoss <- sum(pmax(0, 1 - vY * vQ)**2) + dLambda * (t(vV[-1]) %*% vV[-1])
   return(dLoss)
 }
-
 #' SVM majorization
 #' 
 #' Solve the primal SVM problem using majorization
@@ -43,55 +42,54 @@ QuadLossSVM <- function(mX, vY, vV, dLambda){
 #' @param vW Vector of weights
 #' @param dLambda Ridge penalization parameter lambda
 #' @param dEpsilon Accuracy parameter
-#' @param hinge Type of hinge error (absolute or quadratic)
-#' @param silent Boolean argument, if FALSE, function prints the iterations
+#' @param sHinge Type of hinge error (absolute or quadratic)
+#' @param bSilent Boolean argument, if FALSE, function prints the iterations
 #' @export
 MajSVM <- function(mX, vY, dC = 1, vW = rep(1, ncol(mX) - 1), 
                    dLambda = 1, dEpsilon = 10^(-6),
-                   hinge = c('absolute', 'quadratic'), silent = TRUE){
+                   sHinge = 'absolute', bSilent = TRUE){
   iN <- length(vY)
   iP <- ncol(mX)
   vV0 <- c(dC, vW)                      
-  dStartL <- AbsLossSVM(mX, vY, vV0, dLambda)          #starting SVM loss value
-  dL0 <- dStartL
-  dDecrease <- 0
+  dStartL <- LossSVM(mX, vY, vV0, dLambda)          #starting SVM loss value
   mP <- diag(iP)
   mP[1,1] <- 0
   
-  if(hinge == 'quadratic'){
+  if(sHinge == 'quadratic'){
     mZ <- solve(t(mX) %*% mX + dLambda * mP) %*% t(mX)
+    dStartL <- LossSVMQuadr(mX, vY, vV0, dLambda)          #starting SVM loss value
   }
   
+  dL0 <- dStartL
+  dDecrease <- 0
   iK <- 1
   while((iK == 1) | dDecrease > dEpsilon){
-    
     vQ <- mX %*% vV0  
-    if(hinge == 'absolute'){
-      vA <- ifelse(abs(vQ) > 1.001, 1/(4 * abs(1 - vY * vQ)), dEpsilon/10)   
+  
+    if(sHinge == 'absolute'){
+      vA <- ifelse(abs(1 - vY * vQ) > dEpsilon, 1/(4 * abs(1 - vY * vQ)), 1/(4*dEpsilon))   
       vB <- vY * (vA + 0.25)
       mA <- diag(as.vector(vA))
       vV1 <- solve(t(mX) %*% mA %*% mX  + dLambda * mP, t(mX) %*% vB)
-      dL1 <- AbsLossSVM(mX, vY, vV1, dLambda)
+      dL1 <- LossSVM(mX, vY, vV1, dLambda)
     }
     
-    if(hinge == 'quadratic'){
-      vB <- ifelse((vY == -1 & vQ > -1) | (vY == 1 & vQ > 1), vY * 1, vQ)
+    if(sHinge == 'quadratic'){
+      vB <- ifelse((vY == -1 & vQ > -1) | (vY == 1 & vQ < 1), vY * 1, vQ)
       vV1 <- mZ %*% vB
-      dL1 <- QuadLossSVM(mX, vY, vV1, dLambda)
+      dL1 <- LossSVMQuadr(mX, vY, vV1, dLambda)
     }
     
     dDecrease <- (dL0 - dL1)/dL0
     
-    dL0 <- dL1
-    vV0 <- vV1
-    
-    if (silent == FALSE) {
+    if (bSilent == FALSE) {
       cat("Iteration: ", iK, "Loss SVM: ", dL1, 
           "Relative difference loss SVM: ", dDecrease,"\n")
     }
-    
+    dL0 <- dL1
+    vV0 <- vV1
     iK <- iK + 1
   }
-  return(list('c' = vV0[1], 'w' = vV0[-1], 'startLoss' = dStartL, 'endLoss' = dL0,
-              'iter' = iK, 'q' = vQ))
+  return(list('v' = vV0, 'startLoss' = dStartL, 'endLoss' = dL0,
+              'iter' = iK))
 }
